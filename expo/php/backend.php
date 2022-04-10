324689b6fd3a998ad705b5f9e1194a254ade9bbf
ajenny10 <ajenn958@gmail.com>
2022-04-11 03:08:31 +0530

    add

--- expo/php/backend.php
+++ expo/php/backend.php
@@ -9,10 +9,10 @@ echo '</br> Last Name= ',$lname;
 echo '</br> Email= ',$email;
 echo '</br> Message= ',$message;
 echo '</br> Additional description= ',$description;
-$con=mysqli_connect('localhost','root',',','tour');
-$r=mysqli_query($con,"insert into contacts(fname,lname,email,message,description)values('$fname','$lname','$email',,'$message','$description')");
+$con=mysqli_connect("localhost","root","","tour");
+$r=mysqli_query($con,"insert into contacts(fname,lname,email,message,description)values('$fname','$lname','$email','$message','$description')");
 if($r)
-    echo '</br> Insert successful' 
+    echo '</br> Insert Successfully'; 
     else
     echo mysqli_error($con);
 ?>
