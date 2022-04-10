654ad5f8ee2f2f92a773e449ca85340ac7c47591
ajenny10 <ajenn958@gmail.com>
2022-04-11 02:11:27 +0530

    adding more to php files

--- expo/php backend/contact.php
+++ expo/php backend/contact.php
@@ -1,16 +1,16 @@
 <?php
-$fname=$_POST['fn'];
-$lname=$_POST['ln'];
-$email=$_POST['em'];
-$message=$_POST['msg'];
-$description=$_POST['des'];
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+$description=$_POST['description'];
 echo 'First Name= ',$fname;
 echo '</br> Last Name= ',$lname;
-echo '</br> email= ',$email;
-echo '</br> MESSAGE= ',$message;
+echo '</br> Email= ',$email;
+echo '</br> Message= ',$message;
 echo '</br> Additional description= ',$description;
-$con=mysqli_connect('localhost','root',',','contact_us');
-$r=mysqli_query($con,"insert into students(fname,lname,email,message,description)values('$fname','$lname','$email',,'$message','$description')");
+$con=mysqli_connect('localhost','root',',','tour');
+$r=mysqli_query($con,"insert into contacts(fname,lname,email,message,description)values('$fname','$lname','$email',,'$message','$description')");
 if($r)
     echo '</br> Insert successful' 
     else
